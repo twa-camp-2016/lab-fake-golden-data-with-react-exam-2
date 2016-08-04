a962870a48990e05a0f488489223e4d5c152e7e5
lipeishang <1315789454@qq.com>
2016-08-04 21:23:26 +0800

    finish the left

--- js/app.js
+++ js/app.js
@@ -11,12 +11,24 @@ const App = React.createClass({
             isEditor:!this.state.isEditor
         });
     },
+
+    addElement :function (element) {
+        const elements = this.state.elements;
+        elements.push(element);
+        this.setState({elements});
+    },
+
+    deleteElement :function (index) {
+        const elements = this.state.elements;
+        elements.splice(index, 1);
+        this.setState({elements});
+    },
     render:function(){
         const isEditor = this.state.isEditor;
         return <div>
             <button onClick={this.toggle}>{isEditor? "Previewer" : "Editor"}</button>
             <div className={isEditor ? "" : "Previewer"}>
-                <Editor /> 
+                <Editor onAdd = {this.addElement} elements = {this.state.elements} onDelete = {this.deleteElement}/>
             </div>
             <div className={isEditor ? "" : "hidden"}>
                 <Previewer />
@@ -27,7 +39,33 @@ const App = React.createClass({
 
 const Editor = React.createClass({
     render:function(){
-        return <div>Editor</div>
+        return <div>
+            <Left  elements = {this.props.elements} onDelete = {this.props.onDelete}/>
+            <Right onAdd = {this.props.onAdd}/>
+        </div>
+        },
+});
+
+const Left = React.createClass({
+    remove:function (index) {
+        this.props.onDelete(index);
+    },
+    render:function(){
+        const elements = this.props.elements.map((ele, index) => {
+          return <div key = {index}>
+              <input type = {ele}/>
+              <button onClick={this.remove.bind(this, index)}>X</button>
+          </div>
+        });
+        return <div>
+            {elements}
+        </div>
+        }
+});
+
+const Right = React.createClass({
+    render:function(){
+        return <div>Right</div>
         }
 });
 
