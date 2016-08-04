4823f6a3fdeccd055a5b134fc9a008d9a4949877
李杨帆 <2531349593@qq.com>
2016-08-04 19:59:52 +0800

    add leftpanel

--- js/app.js
+++ js/app.js
@@ -13,14 +13,18 @@ const App = React.createClass({
         counts.push(count);
         this.setState({counts})
 
-        console.log(counts)
+    },
+    deleteCount(index){
+        const counts = this.state.counts;
+        counts.splice(index,1)
+        this.setState({counts})
     },
     render(){
         const isEditor=this.state.isEditor
         return <div>
             <button onClick={this.toggle}>{isEditor?"预览":"编辑"}</button>
             <div className={isEditor?"":"hidden"}>
-                <Left></Left>
+                <Left onDelete={this.deleteCount} counts={this.state.counts}></Left>
                 <Right onAdd={this.addCount}></Right>
             </div>
             <div className={isEditor?"hidden":""}>
@@ -31,8 +35,19 @@ const App = React.createClass({
 });
 
 const Left = React.createClass({
+    remove(index){
+        this.props.onDelete(index);
+    },
     render(){
-        return <div>l</div>
+        const counts=this.props.counts.map((count,index)=>{
+            return <div key={index}>
+                <input type={count}/>
+                <button onClick={this.remove.bind(this,index)}>X</button>
+            </div>
+        })
+        return <div>
+            {counts}
+        </div>
     }
 });
 
