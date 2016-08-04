dd5361c02e8a83c8c4a0ac54296eee078676cd59
SmileDandelion <ning.chn@foxmail.com>
2016-08-04 09:52:05 +0800

    can show editor and previewer compontent

--- js/app.js
+++ js/app.js
@@ -2,7 +2,24 @@ const App = React.createClass({
     render: function () {
         return (<div>
             <button>prevew</button>
+            <Editor/>
+            <Previewer/>
         </div>)
     }
 });
+const Editor = React.createClass({
+    render:function () {
+        return (
+            <div>editor</div>
+        );
+    }
+});
+const Previewer = React.createClass({
+    render:function () {
+        return (
+            <div>previewer</div>
+        );
+    }
+});
+
 ReactDOM.render(<App/>, document.getElementById('content'));
