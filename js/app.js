b6ab72f3cb2411474e2078883833f77391f3257d
lipeishang <1315789454@qq.com>
2016-08-04 20:10:46 +0800

    can transform two parts

--- js/app.js
+++ js/app.js
@@ -1,7 +1,57 @@
 const App = React.createClass({
+
+    getInitialState :function () {
+        return {
+            isEditor : true,
+            elements:[]
+        }
+    },
+    toggle :function () {
+        this.setState({
+            isEditor:!this.state.isEditor
+        })
+    },
     render:function(){
-        return <div>hello</div>
+        const isEditor = this.state.isEditor;
+        return <div>
+            <button onClick={this.toggle}>{isEditor? "Previewer" : "Editor"}</button>
+            <div className={isEditor ? "" : "hidden"}>
+                <Editor />
+                </div>
+            <div className={isEditor ? "hidden" : ""}>
+                <Previewer />
+            </div>
+
+        </div>
         }
 });
+
+const Editor = React.createClass({
+    render:function(){
+        return <div>
+            <Left />
+            <Right />
+        </div>
+        }
+});
+
+const Right = React.createClass({
+    render:function(){
+        return <div>Right</div>
+        }
+});
+
+const Left = React.createClass({
+    render:function(){
+        return <div>Left</div>
+        }
+});
+
+const Previewer = React.createClass({
+    render:function(){
+        return <div>Previewer</div>
+        }
+});
+
 
 ReactDOM.render(<App />,document.getElementById('content'));
