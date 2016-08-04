fa5366041fab2f2839b6d1df5b46e296b7a297b8
banshengbushu <wangdanna1995@outlook.com>
2016-08-04 09:46:20 +0800

    create components

--- js/app.js
+++ js/app.js
@@ -1,1 +1,31 @@
-// TODO
+const App = React.createClass({
+    render:function(){
+        return <div>App</div>
+    }
+});
+
+const Editor = React.createClass({
+    render:function(){
+        return <div>E</div>
+    }
+});
+
+const Previewer = React.createClass({
+    render:function(){
+        return <div>Previewer</div>
+    }
+});
+
+const Right = React.createClass({
+    render:function(){
+        return <div>Right</div>
+    }
+});
+
+const Left = React.createClass({
+    render:function(){
+        return <div>Left</div>
+    }
+});
+
+ReactDOM.render(<App />, document.getElementById('content'));
