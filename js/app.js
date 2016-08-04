0e82de1b850252db4a863c55bfc1829a222ec37a
Zxw854953678 <Zxw854953678@163.com>
2016-08-05 03:34:41 +0800

    add element

--- js/app.js
+++ js/app.js
@@ -1,18 +1,25 @@
 const App = React.createClass({
   getInitialState: function () {
     return {
-      isditor: true
+      isditor: true,
+      elements:[]
     }
   },
   toggle: function () {
     this.setState({isditor: !this.state.isditor});
+  },
+  addElement:function (element) {
+    const elements=this.state.elements;
+    elements.push(element);
+    console.log(elements);
+    this.setState({elements});
   },
   render: function () {
     return (
       <div>
         <button onClick={this.toggle}>{this.state.isditor ? "preview" : "edit"}</button>
         <div className={this.state.isditor ? "" : "hidden"}>
-          <Edit />
+          <Edit onAdd={this.addElement}/>
         </div>
         <div className={this.state.isditor ? "hidden" : ""}>
           <Preview />
@@ -26,7 +33,34 @@ const Edit = React.createClass({
   render: function () {
     return (
       <div>
-        hello
+        <Left />
+        <Right add={this.props.onAdd}/>
+      </div>
+    );
+  }
+});
+
+const Left = React.createClass({
+  render: function () {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+});
+
+const Right = React.createClass({
+  add:function () {
+    const element = $("input[name=elem]:checked").val();
+    this.props.add(element);
+  },
+  render: function () {
+    return (
+      <div>
+        <input type="radio" name="elem" value="text"/>Text
+        <input type="radio" name="elem" value="date"/>Date
+        <button onClick={this.add}>+</button>
       </div>
     );
   }
