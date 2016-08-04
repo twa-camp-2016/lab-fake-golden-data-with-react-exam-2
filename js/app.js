4151234db833365d8cf4abf99b41594ee550f281
lipeishang <1315789454@qq.com>
2016-08-04 21:15:29 +0800

    divide into two parts

--- js/app.js
+++ js/app.js
@@ -1,6 +1,39 @@
 const App = React.createClass({
+    getInitialState :function () {
+        return {
+            isEditor:true,
+            elements:[]
+        }
+    },
+
+    toggle :function () {
+        this.setState({
+            isEditor:!this.state.isEditor
+        });
+    },
     render:function(){
-        return <div>App</div>
+        const isEditor = this.state.isEditor;
+        return <div>
+            <button onClick={this.toggle}>{isEditor? "Previewer" : "Editor"}</button>
+            <div className={isEditor ? "" : "Previewer"}>
+                <Editor /> 
+            </div>
+            <div className={isEditor ? "" : "hidden"}>
+                <Previewer />
+            </div>
+        </div>
+        }
+});
+
+const Editor = React.createClass({
+    render:function(){
+        return <div>Editor</div>
+        }
+});
+
+const Previewer = React.createClass({
+    render:function(){
+        return <div>Previewer</div>
         }
 });
 
