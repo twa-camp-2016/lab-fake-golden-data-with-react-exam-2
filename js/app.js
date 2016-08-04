f4b6b37f060826c136a0803d92c26357beb34a4d
yangyue <m18710434142_1@163.com>
2016-08-04 09:54:54 +0800

    two page

--- js/app.js
+++ js/app.js
@@ -1,6 +1,38 @@
 const App=React.createClass({
+   getInitialState:function () {
+     return{
+         elements:[],
+         isEditor:true
+     }
+   },
+
+    toggle:function () {
+      this.setState({isEditor:!this.state.isEditor});
+    },
+
+    render:function () {
+       const isEditor=this.state.isEditor;
+       return <div>
+           <button onClick={this.toggle}>{isEditor?'Preview':'Editor'}</button>
+           <div>
+               <Editor />
+           </div>
+           <div>
+               <Previewer />
+           </div>
+       </div>
+   }
+});
+
+const Editor = React.createClass({
    render:function () {
-       return <div>hhh</div>
+       return <div></div>
+   }
+});
+
+const Previewer = React.createClass({
+   render:function () {
+       return<div></div>
    }
 });
 
