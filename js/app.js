c8a7bbe2f38699c5c6f79e18806b66a8284d9ead
mahong <1025383143@qq.com>
2016-08-04 09:48:11 +0800

    add app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,47 @@
-// TODO
+var App = React.createClass({
+    getInitialState: function () {
+        return {
+            elements: [],
+            isEditor: true
+        }
+    },
+    isChange: function () {
+        this.setState({isEditor: !this.state.isEditor});
+    },
+    render: function () {
+        const isEditor = this.state.isEditor;
+
+        return (
+            <div>
+                <button onClick={this.isChange}>{isEditor ? 'preview' : 'editor'}</button>
+                <div className={isEditor ? '' : "hidden"}>
+                    <Editor/>
+                </div>
+                <div className={isEditor ? 'hidden' : " "}>
+                    <Preview/>
+                </div>
+
+            </div>
+
+        )
+    }
+});
+
+var Editor = React.createClass({
+    render: function () {
+        return (
+        <div></div>
+           
+        )
+    }
+});
+
+var Preview = React.createClass({
+    render: function () {
+        return (
+        <div></div>
+           
+        )
+    }
+});
+ReactDOM.render(<App/>,document.getElementById('content'));
