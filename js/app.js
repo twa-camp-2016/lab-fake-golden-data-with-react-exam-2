948400f6909731b76cd1b6cb8848a406bb7b37ad
李杨帆 <2531349593@qq.com>
2016-08-04 20:56:53 +0800

    add leftpanel

--- js/app.js
+++ js/app.js
@@ -12,27 +12,41 @@ const App = React.createClass({
         const counts= this.state.counts;
         counts.push(count);
         this.setState({counts});
-
-        console.log(counts)
+    },
+    deleteCount(index){
+        const counts= this.state.counts;
+        counts.splice(index,1)
+        this.setState({counts});
     },
     render(){
         const isEditor=this.state.isEditor
         return <div>
             <button onClick={this.toggle}>{isEditor?"预览":"编辑"}</button>
             <div className={isEditor?"":"hidden"}>
-                <Left></Left>
+                <Left counts={this.state.counts} onDelete={this.deleteCount}></Left>
                 <Right onAdd={this.addCount}></Right>
             </div>
             <div className={isEditor?"hidden":""}>
-                <Preview></Preview>
+                <Preview counts={this.state.counts}></Preview>
             </div>
         </div>
     }
 });
 
 const Left = React.createClass({
+    remove(index){
+        this.props.onDelete(index)
+    },
     render(){
-        return <div></div>
+        const counts=this.props.counts.map((count,index)=>{
+            return <div key={index}>
+                <input type={count}/>
+                <button onClick={this.remove.bind(this,index)}>X</button>
+            </div>
+        })
+        return <div>
+            {counts}
+        </div>
     }
 });
 
@@ -52,7 +66,7 @@ const Right = React.createClass({
 
 const Preview  = React.createClass({
     render(){
-        return <div></div>
+      
     }
 });
 
