f63a5aba4a0ec018d07bbe7f143e8c3a52c28bf0
Zxw854953678 <Zxw854953678@163.com>
2016-08-05 00:48:38 +0800

    implement preview/edit change

--- js/app.js
+++ js/app.js
@@ -1,22 +1,46 @@
 const App = React.createClass({
-    getInitialState: function () {
-      return {isEditor: true};
-    },
-    toggle: function () {
-      this.setState({
-        isEditor: !this.state.isEditor
-      });
-    },
-    render: function () {
-      const isEditor = this.state.isEditor;
-      return (
-        <div>
-          <button onClick={this.toggle}>{isEditor ? "preview" : "edit"}</button>
+  getInitialState: function () {
+    return {isEditor: true};
+  },
+  toggle: function () {
+    this.setState({
+      isEditor: !this.state.isEditor
+    });
+  },
+  render: function () {
+    const isEditor = this.state.isEditor;
+    return (
+      <div>
+        <button onClick={this.toggle}>{isEditor ? "preview" : "edit"}</button>
+        <div className={isEditor ? "" : "hidden"}>
+          <Edit/>
         </div>
-      );
-    }
-  })
-  ;
+        <div className={isEditor ? "hidden" : ""}>
+          <Preview />
+        </div>
+      </div>
+    );
+  }
+});
 
+const Edit = React.createClass({
+  render: function () {
+    return (
+      <div>
+        Edit
+      </div>
+    );
+  }
+});
+
+const Preview = React.createClass({
+  render: function () {
+    return (
+      <div>
+        Preview
+      </div>
+    );
+  }
+});
 
 ReactDOM.render(<App/>, document.getElementById('content'));
