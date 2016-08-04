42f44e6dba7640098f8fe2902cc2ff5ec97d4cb8
chengyurou <18829292695@163.com>
2016-08-03 22:06:07 -0400

    add ele

--- js/app.js
+++ js/app.js
@@ -14,17 +14,22 @@ const App = React.createClass({
         
         this.setState({elements});
     },
-   
+    onRemove:function (i) {
+        const elements = this.state.elements;
+        elements.splice(i,1);
+
+        this.setState({elements});
+    },
     render: function (isEditor) {
         isEditor = isEditor || this.state.isEditor;
         return (
             <div>
                 <button onClick={this.toogle}>{isEditor? 'preview':'edit'}</button>
-                <div className={isEditor?'':'hidden'}>
-                    <Editor onAdd={this.add}/>
+                <div className={isEditor?'':'hidden'} >
+                    <Editor onAdd={this.add} onRemove={this.onRemove} elements={this.state.elements}/>
                 </div>
                 <div className={isEditor?'hidden':''}>
-                    <Preciew/>
+                    <Preciew elements={this.state.elements}/>
                 </div>
             </div>
         )
@@ -37,7 +42,7 @@ const Editor = React.createClass({
         return (
             <div>
                 <div>
-                    <Left/>
+                    <Left onRemove={this.props.onRemove}/>
                 </div>
                 <div>
                     <Right onAdd={this.props.onAdd}/>
@@ -48,10 +53,20 @@ const Editor = React.createClass({
 });
 
 const Left = React.createClass({
+
+    remove:function (i) {
+        this.props.onRemove(i)
+    },
    
     render: function(){
+        const elements = this.props.elements.map((ele,i)=> {
+            return <div key={i}>
+                <input type={ele}/>
+                <button onClick={this.remove.bind(this,i)}>x</button>
+            </div>
+        })
         return (
-            <div>Left</div>
+            <div>{elements}</div>
         )
     }   
 });
@@ -76,8 +91,13 @@ const Right = React.createClass({
 const Preciew = React.createClass({
 
     render: function(){
+        const elements = this.props.elements.map((ele,i)=> {
+            return <div key={i}>
+                <input type={ele}/>
+            </div>
+        })
         return (
-            <div>Preciew</div>
+            <div>{elements}</div>
         )
     }
 });
