cde96a00b5f7e86a7550ba84059d72fb5f8b1668
ElsaDowney <446189226@qq.com>
2016-08-04 20:29:28 +0800

    create Right

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ const App = React.createClass({
     getInitialState: function () {
         return {
             isEditor: true,
-            element: []
+            elements: []
         }
     },
     toggle: function () {
@@ -11,19 +11,40 @@ const App = React.createClass({
             isEditor:!this.state.isEditor
         })
     },
+    addElement:function (element) {
+        const elements = this.state.elements;
+        elements.push(element);
+        this.setState({elements});
+    },
     render: function () {
         const isEditor = this.state.isEditor;
         return <div>
             <button onClick={this.toggle}>{isEditor ? "Editor" : "Preview"}</button>
             <div className={isEditor?"":"hidden"}>
-                <Editor />
+                <Editor elements={this.state.elements} onAdd={this.addElement}/>
             </div>
         </div>
     }
 });
 const Editor = React.createClass({
     render:function () {
-        return <div>111</div>
+        return <div>
+
+            <Right onAdd={this.props.onAdd}/>
+        </div>
+    }
+});
+const Right = React.createClass({
+    add:function () {
+        const element = $("input[name=element]:checked").val();
+        this.props.onAdd(element);
+    },
+    render:function () {
+        return <div>
+            <input type="radio" name="element" value="text" />Text
+            <input type="radio" name="element" value="date" />Date
+            <button onClick={this.add}>+</button>
+        </div>
     }
 });
 
