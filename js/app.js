fa3acf5d8583b96b2b6b6f633752f29262e9d6c8
RangelZZZ <18792561236@163.com>
2016-08-04 10:07:50 +0800

    "finsished add text and date"

--- js/app.js
+++ js/app.js
@@ -11,13 +11,19 @@ const App = React.createClass({
             isEditor: !this.state.isEditor
         })
     },
+    addElement: function (element) {
+        const elements = this.state.elements;
+        elements.push(element);
+        this.setState(elements);
+
+    },
 
     render: function () {
         const isEditor = this.state.isEditor;
         return <div>
             <button onClick={this.toggle}>{isEditor ? "Preview" : "Edit"}</button>
             <div className={isEditor ? "" : "hidden"}>
-                <Editor/>
+                <Editor elements={this.state.elements} onAdd={this.addElement}/>
             </div>
             <div className={isEditor ? "hidden" : ""}>
                 <Preview/>
@@ -29,19 +35,39 @@ const App = React.createClass({
 
 const Editor = React.createClass({
     render: function () {
-        return <div>hah</div>
+        return <div>
+            <Left elements={this.props.elements}/>
+            <Right onAdd={this.props.onAdd}/>
+        </div>
     }
 });
 
 const Left = React.createClass({
+
     render: function () {
-        return <div></div>
+        const elements = this.props.elements.map((ele, index)=> {
+            return <div key={index}>
+                <input type={ele}/>
+                <button>-</button>
+            </div>
+        });
+        return <div>
+            {elements}
+        </div>
     }
 });
 
 const Right = React.createClass({
+    add: function () {
+        const element = $("input[name=element]:checked").val();
+        this.props.onAdd(element)
+    },
     render: function () {
-        return <div></div>
+        return <div>
+            <input type="radio" name="element" value="text"/>text
+            <input type="radio" name="element" value="date"/>date
+            <button onClick={this.add}>+</button>
+        </div>
     }
 });
 const Preview = React.createClass({
