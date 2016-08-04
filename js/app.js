beaad995f4e81d6e96e3215fb730347c23e99a07
ElsaDowney <446189226@qq.com>
2016-08-04 21:30:38 +0800

    create Right

--- js/app.js
+++ js/app.js
@@ -1,25 +1,65 @@
 const App = React.createClass({
-    getInitialState: function () {
+    getInitialState: function() {
         return {
-            isEdtior: true,
+            isEditor: true,
             elements: []
         }
     },
     toggle: function () {
         this.setState({
-            isEdtior: !this.state.isEdtior
-        })
+            isEditor: !this.state.isEditor
+        });
     },
-
-    render: function () {
-        const isEditor = this.state.isEdtior;
+    addElement: function(element) {
+        const elements = this.state.elements;
+        elements.push(element);
+        this.setState({elements});
+    },
+    deleteElement: function (index) {
+        const elements = this.state.elements;
+        elements.splice(index, 1);
+        this.setState({elements});
+    },
+    render: function() {
+        const isEditor = this.state.isEditor;
         return <div>
-            <button onClick={this.toggle}>{isEditor ? "Editor" : "Preview"}</button>
-        </div>
-
+            <button onClick={this.toggle}>{isEditor ? "Preview" : "Edit"}</button>
+            <div className={isEditor ? "" : "hidden"}>
+                <Editor elements={this.state.elements} onAdd={this.addElement} onDelete={this.deleteElement} />
+            </div>
+            <div className={isEditor ? "hidden" : ""}>
+                <Previewer elements={this.state.elements} />
+            </div>
+        </div>;
     }
 });
 
-ReactDOM.render(
-    <App />
-    , document.getElementById('content'));
+const Editor = React.createClass({
+    render: function () {
+        return <div>
+            
+            <Right onAdd={this.props.onAdd}/>
+        </div>;
+    }
+});
+
+
+const Right = React.createClass({
+    add: function () {
+        const element = $("input[name=element]:checked").val();
+        this.props.onAdd(element);
+        // console.log(element)
+    },
+    render: function() {
+        return <div>
+            <input type="radio" name="element" value="text" />Text
+            <input type="radio" name="element" value="date" />Date
+            <button onClick={this.add}>+</button>
+        </div>
+    }
+});
+
+
+
+
+ReactDOM.render(<App />, document.getElementById('content'));
