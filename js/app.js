a4322ac9a664a8a374a472c50692071bdc51c7d7
RangelZZZ <18792561236@163.com>
2016-08-04 23:14:55 +0800

    make the layout

--- js/app.js
+++ js/app.js
@@ -1,10 +1,17 @@
 const App = React.createClass({
   render: function(){
     return <div>
-        hello
+        <Editor/>
     </div>
   }
 });
+const Editor = React.createClass({
+  render: function(){
+    return <div>
+        world
+    </div>
+  }
+})
 
 
 ReactDOM.render(<App/>, document.getElementById('content'));
