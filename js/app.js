651ee9d1b4efabf7a753b4d90cf1e25dd6e02999
code <1165444511@qq.com>
2016-08-04 09:47:37 +0800

    it works

--- js/app.js
+++ js/app.js
@@ -1,1 +1,37 @@
-// TODO
+const App = React.createClass({
+
+render:function(){
+return<div>
+<div>
+    <Editor />
+    <Preview />
+</div></div>
+}
+});
+
+const Editor = React.createClass({
+render:function(){
+return<div>
+<Left />
+< Right/>
+</div>
+}
+})
+
+const Left = React.createClass({
+render:function(){
+return<div> Left</div>
+}
+});
+
+const Right = React.createClass({
+render:function(){
+return<div> Right</div>
+}
+})
+const Preview = React.createClass({
+render:function(){
+return<div> Preview</div>
+}
+})
+ ReactDOM.render(<App />, document.getElementById('content'));
