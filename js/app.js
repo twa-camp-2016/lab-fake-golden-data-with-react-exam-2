4d4053f8aa80f18c0e3cb7ba13b123c3e6d5d36a
RangelZZZ <18792561236@163.com>
2016-08-04 20:52:18 +0800

    finished swtich page

--- js/app.js
+++ js/app.js
@@ -1,8 +1,27 @@
 const App = React.createClass({
+
+    getInitialState: function () {
+        return {
+            isEditor: true,
+            elements: []
+        }
+    },
+
+    toggle: function () {
+        this.setState({
+            isEditor: !this.state.isEditor
+        });
+    },
     render: function () {
+        const isEditor = this.state.isEditor;
         return <div>
-            <Editor/>
-            <Preview/>
+            <button onClick={this.toggle}>{isEditor ? "Preview" : "Edit"}</button>
+            <div className={isEditor ? "" : "preview"}>
+                <Editor/>
+            </div>
+            <div className={isEditor ? "preview" : ""}>
+                <Preview/>
+            </div>
         </div>
     }
 });
