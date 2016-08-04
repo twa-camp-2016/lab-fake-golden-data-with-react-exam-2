1dc3a21da4d2bb5ca1b671f435a37fa7992e04d3
mahong <1025383143@qq.com>
2016-08-04 19:48:16 +0800

    add Editor:Right

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 var App = React.createClass({
-    getInitialState: function(){
+    getInitialState: function () {
         return {
             elements: [],
             isEditor: true
@@ -8,17 +8,72 @@ var App = React.createClass({
     isChange: function () {
         this.setState({isEditor: !this.state.isEditor})
     },
+    add: function (ele) {
+        const elements = this.state.elements;
+        elements.push(ele);
+        this.setState(elements);
+    },
     render: function () {
         const isEditor = this.state.isEditor;
         return (
             <div>
                 <button onClick={this.isChange}>{isEditor ? "preview" : 'editor'}</button>
-                {/*<Edit/>*/}
-                {/*<Preview/>*/}
+                <div className={isEditor ? '' : "hidden"}>
+                    <Editor elements={this.state.elements} onAdd={this.add}/>
+                </div>
+                <div className={isEditor ? 'hidden' : " "}>
+                    <Preview/>
+                </div>
             </div>
 
         )
     }
 });
 
+var Editor = React.createClass({
+    render: function () {
+        return (
+            <div>
+                <Left />
+                <Right onAdd={this.props.onAdd}/>
+            </div>
+
+        )
+    }
+});
+
+var Right = React.createClass({
+    add: function () {
+        const element = $("input[name=input]:checked").val();
+        this.props.onAdd(element);
+    },
+    render: function () {
+        return (
+            <div>
+                <input type="radio" name="input" value="text"/>文本框
+                <input type="radio" name="input" value="text"/>日期
+                <button onClick={this.add}>+</button>
+
+            </div>
+
+        )
+    }
+});
+
+var Left = React.createClass({
+    render: function () {
+        return (
+        <div></div>
+           
+        )
+    }
+});
+var Preview = React.createClass({
+    render: function () {
+        return (
+        <div></div>
+           
+        )
+    }
+});
 ReactDOM.render(<App/>, document.getElementById("content"));
