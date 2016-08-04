7aabcff9fa8d36bb083c6300654b2c8d8cf6d4a2
RangelZZZ <18792561236@163.com>
2016-08-04 09:54:06 +0800

    "finished switch page"

--- js/app.js
+++ js/app.js
@@ -1,1 +1,54 @@
-// TODO
+'use strict';
+const App = React.createClass({
+    getInitialState: function () {
+        return {
+            isEditor: true,
+            elements: []
+        }
+    },
+    toggle: function () {
+        this.setState({
+            isEditor: !this.state.isEditor
+        })
+    },
+
+    render: function () {
+        const isEditor = this.state.isEditor;
+        return <div>
+            <button onClick={this.toggle}>{isEditor ? "Preview" : "Edit"}</button>
+            <div className={isEditor ? "" : "hidden"}>
+                <Editor/>
+            </div>
+            <div className={isEditor ? "hidden" : ""}>
+                <Preview/>
+            </div>
+        </div>
+
+    }
+});
+
+const Editor = React.createClass({
+    render: function () {
+        return <div>hah</div>
+    }
+});
+
+const Left = React.createClass({
+    render: function () {
+        return <div></div>
+    }
+});
+
+const Right = React.createClass({
+    render: function () {
+        return <div></div>
+    }
+});
+const Preview = React.createClass({
+    render: function () {
+        return <div>xix</div>
+    }
+});
+
+
+ReactDOM.render(<App/>, document.getElementById("content"));
