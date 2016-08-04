2e1ae085655a93b0a927e6863e5ac82ef4a0cde7
chengyurou <18829292695@163.com>
2016-08-03 21:51:52 -0400

    charge

--- js/app.js
+++ js/app.js
@@ -1,1 +1,45 @@
-// TODO
+const App = React.createClass({
+    getInitialState:function(){
+        return {
+            isEditor:true
+        }
+    },
+    toogle:function () {
+        this.setState({isEditor:!this.state.isEditor});
+    },
+   
+    render: function (isEditor) {
+        isEditor = isEditor || this.state.isEditor;
+        return (
+            <div>
+                <button onClick={this.toogle}>{isEditor? 'preview':'edit'}</button>
+                <div>
+                    <Editor/>
+                </div>
+                <div>
+                    <Preciew/>
+                </div>
+            </div>
+        )
+    }   
+});
+
+const Editor = React.createClass({
+
+    render: function(){
+        return (
+            <div>Editor</div>
+        )
+    }
+});
+
+const Preciew = React.createClass({
+
+    render: function(){
+        return (
+            <div>Preciew</div>
+        )
+    }
+});
+
+ReactDOM.render(<App/>,document.getElementById('content'))
