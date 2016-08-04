7f9b2ca8a67f8a2da47536736dbf7cdbb2454e07
lipeishang <1315789454@qq.com>
2016-08-04 09:48:42 +0800

    divide two parts

--- js/app.js
+++ js/app.js
@@ -1,7 +1,30 @@
 const App = React.createClass({
+    // getInitialState :function () {
+    //     return {
+    //         isEditor:true,
+    //         elements:[]
+    //     }
+    // },
+
     render:function(){
-        return <div>App</div>
+        // const isEditor = this.state.isEditor;
+        return <div>
+            {/*<button onClick={}></button>*/}
+            <Editor />
+            <Previewer />
+        </div>
         }
 });
 
+const Editor = React.createClass({
+    render:function(){
+        return <div>Editor</div>
+        }
+});
+
+const Previewer = React.createClass({
+    render:function(){
+        return <div>Previewer</div>
+        }
+});
 ReactDOM.render(<App />,document.getElementById('content'));
