a646b1adcb770e288f0a8ae6ec5fcebe06bd99c1
code <1165444511@qq.com>
2016-08-04 10:14:27 +0800

    delete

--- js/app.js
+++ js/app.js
@@ -1,76 +1,79 @@
 const App = React.createClass({
     getInitialState: function () {
-            return {
-                isEditor:true,
-                elements:[]
-            }
-        },
-toggle:function () {
-  this.setState( {
-      isEditor:!this.state.isEditor
-  })
-
-},
-addElement:function (element) {
-  const elements = this.state.elements;
-    elements.push(element);
-    this.setState({elements});
-},
-    deleteElement:function (index) {
-        const elements = this.state.elements;
-        elements.splice(index,1);
-        this.setSate({elements});
+        return {
+            isEditor: true,
+            elements: []
+        }
     },
-render:function(){
-            const isEditor = this.state.isEditor;
-return<div>
-    <button onClick={this.toggle}>{isEditor? "Preview":"Editor"}</button>
-<div className={isEditor? "" :"hidden"}>
-    <Editor onAdd={this.addElement} elements = {this.state.elements} onDelete={this.deleteElement}/>
-    <div className={isEditor? "hidden":""}>
-        <Preview /></div>
+    toggle: function () {
+        this.setState({
+            isEditor: !this.state.isEditor
+        })
 
-</div></div>
-}
+    },
+    addElement: function (element) {
+        const elements = this.state.elements;
+        elements.push(element);
+        this.setState({elements});
+    },
+    deleteElement: function (index) {
+        const elements = this.state.elements;
+        elements.splice(index, 1);
+        this.setState({elements});
+    },
+    render: function () {
+        const isEditor = this.state.isEditor;
+        return <div>
+            <button onClick={this.toggle}>{isEditor ? "Preview" : "Editor"}</button>
+            <div className={isEditor ? "" : "hidden"}>
+                <Editor onAdd={this.addElement} elements={this.state.elements} onDelete={this.deleteElement}/>
+                <div className={isEditor ? "hidden" : ""}>
+                    <Preview /></div>
+
+            </div>
+        </div>
+    }
 });
 
 const Editor = React.createClass({
-render:function(){
-return<div>
-<Left  elements = {this.props.elements} onDelete={this.props.onDelete}/>
-< Right onAdd={this.props.onAdd}/>
-</div>
-}
+    render: function () {
+        return <div>
+            <Left elements={this.props.elements} onDelete={this.props.onDelete}/>
+            < Right onAdd={this.props.onAdd}/>
+        </div>
+    }
 })
 
 const Left = React.createClass({
-    remove:function (index) {
-      this.props.onDelete(index);
+    remove: function (index) {
+        this.props.onDelete(index);
     },
-render:function(){
-    const elements= this.props.elements.map((ele,index)=>{
-       return <div><input type={ele}/>
-       <button onClick={this.remove.bind(this,index)}>X</button></div>
-    })
-return<div> {elements}</div>
-}
+    render: function () {
+        const elements = this.props.elements.map((ele, index)=> {
+            return <div><input type={ele}/>
+                <button onClick={this.remove.bind(this, index)}>X</button>
+            </div>
+        })
+        return <div> {elements}</div>
+    }
 });
 
 const Right = React.createClass({
-    add:function () {
-      const element = $('input[name=element]:checked').val();
+    add: function () {
+        const element = $('input[name=element]:checked').val();
         this.props.onAdd(element);
     },
-render:function(){
-return<div>
-    <input type="radio" name="element" value="text" /> Text
-     <input type="radio" name="element" value="date"/>Date
-<button onClick={this.add} ick>+</button></div>
-}
+    render: function () {
+        return <div>
+            <input type="radio" name="element" value="text"/>Text
+            <input type="radio" name="element" value="date"/>Date
+            <button onClick={this.add}>+</button>
+        </div>
+    }
 })
 const Preview = React.createClass({
-render:function(){
-return<div> Preview</div>
-}
+    render: function () {
+        return <div> Preview</div>
+    }
 })
- ReactDOM.render(<App />, document.getElementById('content'));
+ReactDOM.render(<App />, document.getElementById('content'));
