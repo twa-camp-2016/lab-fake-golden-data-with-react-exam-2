29e6f5f0a5ed88d434694cb8e140c00779a9aab7
RangelZZZ <18792561236@163.com>
2016-08-04 22:52:44 +0800

    finished  add elements

--- js/app.js
+++ js/app.js
@@ -12,14 +12,20 @@ const App = React.createClass({
         })
     },
 
+    addElement: function (element) {
+        const elements = this.state.elements;
+        elements.push(element);
+        this.setState({elements});
+    },
+
     render: function () {
         const isEditor = this.state.isEditor;
         return <div>
             <button onClick={this.toggle}>{isEditor ? "Preview" : "Edit"}</button>
-            <div className={isEditor ? "hidden" :""}>
-                <Editor/>
+            <div className={isEditor ? "hidden" : ""}>
+                <Editor elements={this.state.elements} onAdd={this.addElement}/>
             </div>
-            <div className={isEditor ? "" :"hidden"}>
+            <div className={isEditor ? "" : "hidden"}>
                 <Preview/>
             </div>
         </div>
@@ -28,7 +34,42 @@ const App = React.createClass({
 const Editor = React.createClass({
     render: function () {
         return <div>
-            hello
+            <Left elements={this.props.elements}/>
+            <Right onAdd={this.props.onAdd}/>
+        </div>
+
+    }
+});
+
+
+const Left = React.createClass({
+
+    render: function () {
+        const elements = this.props.elements.map((ele, index) => {
+            return <div key={index}>
+                <input type={ele}/>
+                <button>-</button>
+            </div>
+        });
+
+        return <div>
+            {elements}
+        </div>
+    }
+});
+
+const Right = React.createClass({
+
+    add: function () {
+        const element =  $("input[name=element]:checked").val();
+        this.props.onAdd(element);
+    },
+
+    render: function () {
+        return <div>
+            <input type="radio" name="element" value="text"/>text
+            <input type="radio" name="element" value="date"/>date
+            <button onClick={this.add}>+</button>
         </div>
     }
 });
