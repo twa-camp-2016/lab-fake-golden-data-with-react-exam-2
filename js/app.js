48e87a7050d879da432f61a359ed8ec0f58b1ba7
RangelZZZ <18792561236@163.com>
2016-08-05 00:37:16 +0800

    finished preview

--- js/app.js
+++ js/app.js
@@ -28,7 +28,7 @@ const App = React.createClass({
                 <Editor elements={this.state.elements} onAdd={this.addElement} ondelete={this.deleteElement}/>
             </div>
             <div className={isEditor ? "hidden" : ""}>
-                <Preview/>
+                <Preview elements ={this.state.elements}/>
             </div>
 
         </div>
@@ -75,7 +75,15 @@ const Right = React.createClass({
 })
 const Preview = React.createClass({
     render: function () {
-        return <div>xox</div>
+        const elements = this.props.elements.map((ele, index)=> {
+            return <div key={index}>
+                <input type={ele}/>
+            </div>
+        });
+        return <div>
+            {elements}
+            <button>submit</button>
+        </div>
     }
 });
 ReactDOM.render(<App/>, document.getElementById('content'));
