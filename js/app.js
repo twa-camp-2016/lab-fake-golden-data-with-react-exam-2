03c8352880f55732ef784aeadd8681c61bf36fbe
code <1165444511@qq.com>
2016-08-04 21:13:27 +0800

    achieve two page's turning

--- js/app.js
+++ js/app.js
@@ -1,10 +1,20 @@
 const App = React.createClass({
+    getInitialState: function () {
+            return {
+               isEditor:true,
+                elements:[]
+            }
+        },
+    toggle:function () {
+        this.setState({isEditor:!this.state.isEditor});
+    },
     render: function () {
-        return <div>
-            <div>
+            const isEditor = this.state.isEditor;
+        return <div><button onClick={this.toggle}>{isEditor?"Preview":"Edit"}</button>
+            <div className={isEditor?"":"hidden"}>
                 <Editor />
             </div>
-            <div>
+            <div className={isEditor?"hidden":""}>
                 <Preview />
             </div>
         </div>
