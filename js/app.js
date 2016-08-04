7bfb034505f04d928a7ff4da3c6b63e3a15d3d3b
RangelZZZ <18792561236@163.com>
2016-08-04 20:39:36 +0800

    make the lauout

--- js/app.js
+++ js/app.js
@@ -1,9 +1,25 @@
 const App = React.createClass({
-    render: function(){
-    return <div>
-        hello
-    </div>
-  }
+    render: function () {
+        return <div>
+            <Editor/>
+            <Preview/>
+        </div>
+    }
+});
+const Editor = React.createClass({
+    render: function () {
+        return <div>
+            haha
+        </div>
+    }
+});
+
+const Preview = React.createClass({
+    render: function () {
+        return <div>
+            hhh
+        </div>
+    }
 });
 
 ReactDOM.render(<App/>, document.getElementById('content'));
