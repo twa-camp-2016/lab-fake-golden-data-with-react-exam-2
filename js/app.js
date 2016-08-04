eaa0d724b92fc315f1070b2e406b1396e5be03f7
ElsaDowney <446189226@qq.com>
2016-08-04 09:53:10 +0800

    new Button

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
-// TODO
+const App = React.createClass({
+    getInitialState:function () {
+        return {
+            isEditor:true,
+            element:[]
+        }
+    },
+    toggle:function () {
+        this.setState({
+            isEditor:!this.state.isEditor
+        })
+    },
+    render: function () {
+        const isEditor=this.state.isEditor;
+        return <div>
+            <button onClick={this.toggle}>{isEditor ? "Preview" : "Editor"}</button>
+        </div>
+    }
+});
+
+ReactDOM.render(<App />, document.getElementById('content'));
