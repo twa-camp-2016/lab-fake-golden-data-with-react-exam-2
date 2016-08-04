8cd7cec71cb0ff746822e06790ab5a2daa2ed3a1
Zxw854953678 <Zxw854953678@163.com>
2016-08-05 02:31:29 +0800

    add Text/Date button

--- js/app.js
+++ js/app.js
@@ -2,13 +2,41 @@ const App = React.createClass({
   getInitialState: function () {
     return {isEditor: true};
   },
-  toggle:function () {
-    this.setState({isEditor:!this.state.isEditor});
+  toggle: function () {
+    this.setState({isEditor: !this.state.isEditor});
   },
   render: function () {
     return (
       <div>
         <button onClick={this.toggle}>{this.state.isEditor ? "preview" : "edit"}</button>
+        <div className={this.state.isEditor ? "" : "hidden"}>
+          <Edit/>
+        </div>
+        <div className={this.state.isEditor ? "hidden" : ""}>
+          <Preview/>
+        </div>
+      </div>
+    );
+  }
+});
+
+const Edit = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <input type="radio" name="elem" value="text"/>Text
+        <input type="radio" name="elem" value="date"/>Date
+        <button>+</button>
+      </div>
+    );
+  }
+});
+
+const Preview = React.createClass({
+  render: function () {
+    return (
+      <div>
+        world
       </div>
     );
   }
