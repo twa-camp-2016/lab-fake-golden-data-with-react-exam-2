e95bc94f94886df0adec5f7c1b8c735c25591998
code <1165444511@qq.com>
2016-08-04 09:54:00 +0800

    two page turn

--- js/app.js
+++ js/app.js
@@ -1,10 +1,25 @@
 const App = React.createClass({
+    getInitialState: function () {
+            return {
+                isEditor:true
+            }
+        },
+toggle:function () {
+  this.setState( {
+      isEditor:!this.state.isEditor
+  })
+
+},
 
 render:function(){
+            const isEditor = this.state.isEditor;
 return<div>
-<div>
+    <button onClick={this.toggle}>{isEditor? "Preview":"Editor"}</button>
+<div className={isEditor? "" :"hidden"}>
     <Editor />
-    <Preview />
+    <div className={isEditor? "hidden":""}>
+        <Preview /></div>
+
 </div></div>
 }
 });
