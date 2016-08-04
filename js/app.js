4cd4aebe9b06a112b31574919a95ba578e57ca02
jiaoqiqi <1176932165@qq.com>
2016-08-04 09:50:34 +0800

    it works

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-// TODO
+const App = React.createClass({
+    render:function(){
+        return <div>
+            hello
+        </div>
+
+
+    }
+})
+
+ReactDOM.render(<App />, document.getElementById('content'));
