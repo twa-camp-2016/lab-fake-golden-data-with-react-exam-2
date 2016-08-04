878aff84135e6ed5ab3a7b7863703b6714bd0213
RangelZZZ <18792561236@163.com>
2016-08-05 00:06:12 +0800

    mal=ke the layout

--- js/app.js
+++ js/app.js
@@ -1,7 +1,11 @@
 const App = React.createClass({
   render: function(){
-    return <div> hello</div>
+    return <div> <Editor/></div>
   }
 });
-
+const Editor = React.createClass({
+  render: function(){
+    return <div> hello</div>
+  }
+})
 ReactDOM.render(<App/>,document.getElementById('content'))
