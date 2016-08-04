dbcf1fdff6051f59f726d2b37bf344aea6f8624e
banshengbushu <wangdanna1995@outlook.com>
2016-08-04 10:12:35 +0800

    create Left components

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ const App = React.createClass({
     getInitialState:function () {
       return{
           isEditor:true,
+          elements:[]
       }
     },
 
@@ -10,15 +11,21 @@ const App = React.createClass({
             isEditor: !this.state.isEditor
         });
     },
+
+    addElement:function (element) {
+        const elements = this.state.elements;
+        elements.push(element);
+        this.setState({elements});
+    },
     render:function(){
         const isEditor = this.state.isEditor;
         return <div>
             <button onClick={this.toggle}>{isEditor?"Preview":"Edit"}</button>
             <div className={isEditor?"":"hidden"}>
-            <Editor />
+            <Editor onAdd={this.addElement} elements={this.state.elements}/>
                 </div>
             <div className={isEditor?"hidden":""}>
-            <Previewer />
+            <Previewer elements={this.state.elements}/>
                 </div>
         </div>
     }
@@ -27,8 +34,8 @@ const App = React.createClass({
 const Editor = React.createClass({
     render:function(){
         return <div>
-            <Left />
-            <Right />
+            <Left elements={this.props.elements}/>
+            <Right onAdd = {this.props.onAdd}/>
         </div>
     }
 });
@@ -40,18 +47,37 @@ const Previewer = React.createClass({
 });
 
 const Right = React.createClass({
+
+    add:function () {
+        const element = $("input[name=element]:checked").val();
+        this.props.onAdd(element);
+
+    },
+
     render:function(){
         return <div>
             <input type="radio" name="element" value="text"/>Text
             <input type="radio" name="element" value="date"/>Date
-            <button>+</button>
+            <button onClick={this.add}>+</button>
         </div>
     }
 });
 
 const Left = React.createClass({
     render:function(){
-        return <div>Left</div>
+        const elements=this.props.elements.map((ele,index)=>{
+            if(ele!==undefined) {
+                return <div key={index}>
+                    <input type={ele}/>
+                    <button>-</button>
+                </div>
+            }
+        });
+
+        return<div>
+            {elements}
+        </div>
+
     }
 });
 
