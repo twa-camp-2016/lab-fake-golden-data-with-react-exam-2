aee2259d06ba84828e9b1d8e64bb1731c151430b
code <1165444511@qq.com>
2016-08-04 21:36:09 +0800

    init components

--- js/app.js
+++ js/app.js
@@ -1,7 +1,38 @@
 const App = React.createClass({
-render:function(){
-return<div> App</div>
-}
+    render: function () {
+        return <div>
+            <div>
+                <Editor />
+            </div>
+            <div>
+                <Preview />
+            </div>
+        </div>
+    }
+});
+const Editor = React.createClass({
+    render: function () {
+        return <div>
+            <Left />
+            <Right />
+        </div>
+    }
+});
+const Preview = React.createClass({
+    render: function () {
+        return <div> Preview</div>
+    }
 });
 
- ReactDOM.render(<App />, document.getElementById('content'));
+const Left = React.createClass({
+    render: function () {
+        return <div> Left</div>
+    }
+});
+
+const Right = React.createClass({
+    render: function () {
+        return <div> Right</div>
+    }
+})
+ReactDOM.render(<App />, document.getElementById('content'));
