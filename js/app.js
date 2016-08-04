2c16f9647eaa2abf5e7bd904e77957a24d5382bd
mahong <1025383143@qq.com>
2016-08-05 00:15:52 +0800

    Editor:right

--- js/app.js
+++ js/app.js
@@ -8,13 +8,19 @@ var App = React.createClass({
     isChange:function () {
         this.setState({isEditor:!this.state.isEditor})
     },
+    add:function (e) {
+        const elements=this.state.elements;
+        elements.push(e)
+        this.setState(elements);
+
+    },
     render: function () {
         const isEditor=this.state.isEditor;
         return (
             <div>
                 <button onClick={this.isChange}>{isEditor?'preview':'edit'}</button>
                 <div className={isEditor?'':"hidden"}>
-                    <Editor/>
+                    <Editor onAdd={this.add}/>
                     </div>
                 <div className={isEditor?'hidden':" "}>
                     <Preview/>
@@ -28,12 +34,40 @@ var App = React.createClass({
 var Editor = React.createClass({
     render: function () {
         return (
-        <div></div>
+        <div>
+            <Left/>
+            <Right  onAdd={this.props.onAdd}/>
+        </div>
            
         )
     }
 });
 
+var Right = React.createClass({
+    add:function () {
+        const  e=$("input[name=input]:checked").val();
+        this.props.onAdd(e);
+    },
+    render: function () {
+        return (
+        <div>
+            <input type="radio" name="input" value="text"/>文本框
+            <input type="radio" name="input" value="date"/>日期
+            <button onClick={this.add}>+</button>
+        </div>
+
+
+        )
+    }
+});
+var Left = React.createClass({
+    render: function () {
+        return (
+        <div></div>
+
+        )
+    }
+});
 var Preview = React.createClass({
     render: function () {
         return (
