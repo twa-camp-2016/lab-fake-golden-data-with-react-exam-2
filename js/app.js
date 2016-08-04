041109d578666fedbaecfa6ebbe975a0edef074a
code <1165444511@qq.com>
2016-08-04 20:11:26 +0800

    init these component

--- js/app.js
+++ js/app.js
@@ -1,6 +1,40 @@
 const App = React.createClass({
 render:function(){
-return<div> App</div>
+return<div> 
+<div>
+    <Editor />
+</div>
+    <div>
+        <Preview />
+    </div>
+</div>
+}
+});
+
+const Editor = React.createClass({
+render:function(){
+return<div> 
+<Left />
+<Right />
+</div>
+}
+});
+
+const Preview = React.createClass({
+render:function(){
+return<div> Preview</div>
+}
+});
+
+const Left = React.createClass({
+render:function(){
+return<div> Left</div>
+}
+});
+
+const Right = React.createClass({
+render:function(){
+return<div> Right</div>
 }
 })
 
