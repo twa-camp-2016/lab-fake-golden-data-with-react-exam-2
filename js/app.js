e04c9f448bb9b560590c32f9d47458a486ce4d25
SmileDandelion <ning.chn@foxmail.com>
2016-08-04 09:48:42 +0800

    can show

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
-// TODO
+const App = React.createClass({
+    render: function () {
+        return (<div>
+            <button>prevew</button>
+        </div>)
+    }
+});
+ReactDOM.render(<App/>, document.getElementById('content'));
