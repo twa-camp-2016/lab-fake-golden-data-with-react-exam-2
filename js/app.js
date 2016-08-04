1db2656384812dd95f600f7146392a78ae503690
qmm <qmmhao@sina.com>
2016-08-04 23:48:23 +0800

    can run

---
+++ js/app.js
@@ -0,0 +1,11 @@
+const App = React.createClass({
+    render: function () {
+        return <div>
+            <button>Preview</button>
+    }
+});
+
+
+
+
+ReactDOM.render(<App />,document.getElementById("content"));
