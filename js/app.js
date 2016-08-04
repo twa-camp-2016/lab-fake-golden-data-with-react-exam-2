168d2e8a581178a24ed3d496e9ac51de782eaca4
preservance717 <GL717@outlook.com>
2016-08-04 10:00:00 +0800

    add field

--- js/app.js
+++ js/app.js
@@ -1,16 +1,21 @@
 const App = React.createClass({
     getInitialState: function () {
-        return {isEditor: true}
+        return {isEditor: true, elements: []}
     },
 
     onChange: function () {
         this.setState({isEditor: !this.state.isEditor})
     },
+    addElement: function (element) {
+        this.state.elements.push(element);
+        this.setState(this.state.elements);
+    },
 
     render: function () {
         return <div>
             <button onClick={this.onChange}>{this.state.isEditor ? 'Preview' : 'Edit'}</button>
-            <div className={this.state.isEditor ? '' : 'hidden'}><Editor/></div>
+            <div className={this.state.isEditor ? '' : 'hidden'}>
+                <Editor elements={this.state.elements} onAdd={this.addElement}/></div>
             <div className={this.state.isEditor ? 'hidden' : ''}><Preview /></div>
         </div>
     }
@@ -18,9 +23,39 @@ const App = React.createClass({
 
 const Editor = React.createClass({
     render: function () {
-        return <div>Editor</div>
+        return <div>
+            <Right onAdd={this.props.onAdd}/>
+            <Left elements={this.props.elements}/>
+        </div>
     }
 });
+
+const Right = React.createClass({
+    add: function () {
+        const element = $('input[name=input]:checked').val();
+        this.props.onAdd(element);
+    },
+    render: function () {
+        return <div>
+            <input type="radio" name="input" value="text"/>Text
+            <input type="radio" name="input" value="date"/>Date
+            <button onClick={this.add}>+</button>
+        </div>
+    }
+});
+
+const Left = React.createClass({
+    render: function () {
+        return <div>
+            {this.props.elements.map((ele, index)=> {
+                return <div key={index}><input type={ele}/>
+                    <button>-</button>
+                </div>
+            })}
+        </div>
+    }
+});
+
 const Preview = React.createClass({
     render: function () {
         return <div>Preview</div>
