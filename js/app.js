dee6130e5fc322e960e2ebd46cf8ae871452c905
qmm <qmmhao@sina.com>
2016-08-04 23:52:23 +0800

    add Editor component

--- js/app.js
+++ js/app.js
@@ -2,10 +2,26 @@ const App = React.createClass({
     render: function () {
         return <div>
             <button>Preview</button>
+            <div>
+                <Editor />
+            </div>
+        </div>
+
     }
 });
 
 
+const Editor = React.createClass({
+
+    render:function(){
+        return <div>
+            Editor
+        </div>
+    }
+})
 
 
-ReactDOM.render(<App />,document.getElementById("content"));
+
+
+
+ReactDOM.render(<App />, document.getElementById("content"));
