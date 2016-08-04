3d809e94176eeef9a48ffa2de52868db5986b183
Zxw854953678 <Zxw854953678@163.com>
2016-08-05 03:02:33 +0800

    add preview/edit button

--- js/app.js
+++ js/app.js
@@ -1,11 +1,46 @@
 const App = React.createClass({
-    render: function () {
-      return (
-        <div>
-          hello
-        </div>
-      );
+  getInitialState: function () {
+    return {
+      isEditor: true
     }
+  },
+  toggle: function () {
+    this.setState({isEditor: !this.state.isEditor});
+  },
+  render: function () {
+    return (
+      <div>
+        <button onClick={this.toggle}>{this.state.isEditor ? "preview" : "edit"}</button>
+        <div className={this.state.isEditor ? "" : "hidden"}>
+          <Edit />
+        </div>
+        <div className={this.state.isEditor ? "hidden" : ""}>
+          <Preview />
+        </div>
+
+      </div>
+    );
+  }
 });
 
-ReactDOM.render(<App/>,document.getElementById('content'));
+const Edit = React.createClass({
+  render: function () {
+    return (
+      <div>
+        hello
+      </div>
+    );
+  }
+});
+
+const Preview = React.createClass({
+  render: function () {
+    return (
+      <div>
+        world
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(<App/>, document.getElementById('content'));
