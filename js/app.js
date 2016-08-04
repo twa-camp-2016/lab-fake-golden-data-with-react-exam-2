3de9acbb8242c73ac5280e123b55c222814ae684
RangelZZZ <18792561236@163.com>
2016-08-04 22:22:59 +0800

    page can switch

--- js/app.js
+++ js/app.js
@@ -1,16 +1,44 @@
 const App = React.createClass({
-  render: function(){
-    return <div>
-        <Editor/>
-    </div>
-  }
+
+    getInitialState: function () {
+        return {
+            isEditor: true,
+            elements: []
+        }
+    },
+    toggle: function () {
+        this.setState({
+            isEditor: !this.state.isEditor
+        })
+    },
+
+    render: function () {
+        const isEditor = this.state.isEditor;
+        return <div>
+            <button onClick={this.toggle}>{isEditor ? "Preview" : "Edit"}</button>
+            <div className={isEditor ? "hidden" :""}>
+                <Editor/>
+            </div>
+            <div className={isEditor ? "" :"hidden"}>
+                <Preview/>
+            </div>
+        </div>
+    }
 });
 const Editor = React.createClass({
-  render: function(){
-    return <div>
-        hello
-    </div>
-  }
+    render: function () {
+        return <div>
+            hello
+        </div>
+    }
+});
+
+const Preview = React.createClass({
+    render: function () {
+        return <div>
+            llel
+        </div>
+    }
 });
 
 ReactDOM.render(<App/>, document.getElementById('content'));
