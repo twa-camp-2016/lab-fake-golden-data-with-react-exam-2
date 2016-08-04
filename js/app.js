3d8aeb409f52ef0c0dd1a9e2598b66b40ff57f03
jiaoqiqi <1176932165@qq.com>
2016-08-04 20:56:50 +0800

    add initstate

--- js/app.js
+++ js/app.js
@@ -1,8 +1,25 @@
 const App = React.createClass({
+    getInitialState:function () {
+        return {
+            isEditor:true,
+            elements:[]
+        }
+    },
+    toggle:function () {
+        this.setState = {
+            isEditor:!this.state.isEditor
+        }
+
+    },
+
     render :function (){
-        "use strict";
-        return <div>hello</div>
+        const isEditor = this.state.isEditor;
+        return <div>
+            <button onclick={this.toggle}>{isEditor?"Previwer":"Edit"}</button>
+
+        </div>
     }
-})
+});
+
 
 ReactDOM.render(<App/>,document.getElementById('content'))
