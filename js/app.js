3af20978f6012c551190d053801dfe4471107113
preservance717 <GL717@outlook.com>
2016-08-04 10:05:57 +0800

    genenerate Preview page

--- js/app.js
+++ js/app.js
@@ -21,7 +21,7 @@ const App = React.createClass({
             <button onClick={this.onChange}>{this.state.isEditor ? 'Preview' : 'Edit'}</button>
             <div className={this.state.isEditor ? '' : 'hidden'}>
                 <Editor elements={this.state.elements} onAdd={this.addElement} onDelete={this.removeElement}/></div>
-            <div className={this.state.isEditor ? 'hidden' : ''}><Preview /></div>
+            <div className={this.state.isEditor ? 'hidden' : ''}><Preview elements={this.state.elements}/></div>
         </div>
     }
 });
@@ -67,7 +67,11 @@ const Left = React.createClass({
 
 const Preview = React.createClass({
     render: function () {
-        return <div>Preview</div>
+        return <div>{this.props.elements.map((ele, index)=>{
+            return <div key={index}>
+                <input type={ele}/>
+            </div>
+        })}<button>submit</button></div>
     }
 });
 
