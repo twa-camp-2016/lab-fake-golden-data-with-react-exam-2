02b4cabe087f47dcd12c36b235169e0e12a037f8
preservance717 <GL717@outlook.com>
2016-08-04 20:58:10 +0800

    toggle page

--- js/app.js
+++ js/app.js
@@ -1,6 +1,34 @@
 const App = React.createClass({
-    render:function(){
-        return <div>App</div>
+    getInitialState: function () {
+        return {isEditor: true}
+    },
+    handleChange: function () {
+        this.setState({isEditor: !this.state.isEditor});
+    },
+    render: function () {
+        var editor = this.state.isEditor;
+        return <div>
+            <button onClick={this.handleChange}>{editor?'Preview':'Edit'}</button>
+            <div className={editor?'':'hidden'}>
+                <Editor />
+            </div>
+            <div className={editor?'hidden':''}>
+                <Preview />
+            </div>
+        </div>
     }
 });
-ReactDOM.render(<App/>,document.getElementById('content'));
+
+const Editor = React.createClass({
+    render:function(){
+        return <div>Editor</div>
+    }
+});
+
+const Preview = React.createClass({
+    render:function(){
+        return <div>Preview</div>
+    }
+});
+
+ReactDOM.render(<App/>, document.getElementById('content'));
