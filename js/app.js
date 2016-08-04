132dc461988f5e142cce539eb035a3f781933926
Zxw854953678 <Zxw854953678@163.com>
2016-08-05 00:11:53 +0800

    create App

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
-// TODO
+const App = React.createClass({
+  render: function () {
+    return (
+      <div>
+        hello
+      </div>
+    );
+  }
+});
+
+
+ReactDOM.render(<App/>, document.getElementById('content'));
