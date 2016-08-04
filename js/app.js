e0931bc671600f11b37729c006bbd642e5b1c79b
lipeishang <1315789454@qq.com>
2016-08-04 22:17:43 +0800

    finish the right

--- js/app.js
+++ js/app.js
@@ -11,12 +11,18 @@ const App = React.createClass({
             isEditor:!this.state.isEditor
         })
     },
+
+    addElement : function (element) {
+        const elements = this.state.elements;
+        elements.push(element);
+        this.setState({elements});
+    },
     render:function(){
         const isEditor = this.state.isEditor;
         return <div>
             <button onClick={this.toggle}>{isEditor ? "Previewer" : "Editor"}</button>
             <div className={isEditor ? "" : "hidden"}>
-                <Editor />
+                <Editor onAdd = {this.addElement}/>
             </div>
             <div className={isEditor ? "hidden" : ""}>
                 <Previewer />
@@ -27,7 +33,30 @@ const App = React.createClass({
 
 const Editor = React.createClass({
     render:function(){
-        return <div>Editor</div>
+        return <div>
+            <Right onAdd = {this.props.onAdd}/>
+            <Left />
+        </div>
+        }
+});
+
+const Right = React.createClass({
+    add : function () {
+         const element = $("input[name=element]:checked").val();
+        this.props.onAdd(element);
+    },
+    render:function(){
+        return <div>
+            <input type="radio" name="element" value="text"/>Text
+            <input type="radio" name="element" value="date"/>Date
+            <button onClick={this.add}>+</button>
+        </div>
+        }
+});
+
+const Left = React.createClass({
+    render:function(){
+        return <div>Left</div>
         }
 });
 
