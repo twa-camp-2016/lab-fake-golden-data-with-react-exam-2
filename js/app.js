5d4809116a019888ee701d017945d248156f25b0
mahong <1025383143@qq.com>
2016-08-04 21:06:11 +0800

    Editor:right

--- js/app.js
+++ js/app.js
@@ -8,13 +8,19 @@ var App = React.createClass({
     change: function () {
         this.setState({isEditor: !this.state.isEditor})
     },
+    add:function (ele) {
+        const elements=this.state.elements;
+        elements.push(ele);
+        this.setState(elements);
+
+    },
     render: function () {
         const isEditor = this.state.isEditor;
         return (
             <div>
                 <button onClick={this.change}>{isEditor ? "preview" : "edit"}</button>
                 <div className={isEditor ? " " : "hidden"}>
-                    <Editor/>
+                    <Editor onAdd={this.add} onDelete={this.delete} elements={this.state.elements}/>
                 </div>
                 <div className={isEditor ? "hidden " : ""}>
                     <Preview/>
@@ -28,8 +34,37 @@ var App = React.createClass({
 var Editor = React.createClass({
     render: function () {
         return (
-            <div></div>
+            <div>
+                <Right onAdd={this.props.onAdd}/>
+                <Left/>
+            </div>
 
+        )
+    }
+});
+
+var Right = React.createClass({
+    add:function () {
+        const ele=$("input[name=input]:checked").val();
+        this.props.onAdd(ele);
+    },
+    render: function () {
+        return (
+        <div>
+            <input type="radio" name="input" value="text"/>文本框
+            <input type="radio" name="input" value="date"/>日期
+            <button onClick={this.add}>+</button>
+        </div>
+
+
+        )
+    }
+});
+var Left = React.createClass({
+    render: function () {
+        return (
+        <div></div>
+           
         )
     }
 });
