3f4865aca0b045998dc8fc7e7425ba8bdb16e8fe
preservance717 <GL717@outlook.com>
2016-08-04 21:35:56 +0800

    add field

--- js/app.js
+++ js/app.js
@@ -1,17 +1,21 @@
 const App = React.createClass({
     getInitialState:function () {
-        return {isEditor:true}
+        return {isEditor:true, elements:[]}
     },
     handleChange:function () {
         this.setState({isEditor:!this.state.isEditor})
+    },
+    addEle:function (ele) {
+        this.state.elements.push(ele);
+        this.setState(this.state.elements);
     },
     render:function(){
         return <div>
             <button onClick={this.handleChange}>{this.state.isEditor? 'Preview':'Edit'}</button>
             <div className={this.state.isEditor ? '':'hidden'}>
-                <Editor/>
+                <Editor elements={this.state.elements} onAdd={this.addEle}/>
             </div>
-            <div className={this.state.isEditor ? 'hidden':'hidden'}>
+            <div className={this.state.isEditor ? 'hidden':''}>
                 <Preview/>
             </div>
         </div>
@@ -20,7 +24,37 @@ const App = React.createClass({
 
 const Editor = React.createClass({
     render:function(){
-        return <div>Editor</div>
+        return <div>
+            <Right onAdd={this.props.onAdd}/>
+            <Left elements={this.props.elements}/>
+        </div>
+    }
+});
+
+const Right = React.createClass({
+    add:function () {
+      const ele = $('input[name=input]:checked').val();
+        this.props.onAdd(ele);
+    },
+    render:function(){
+        return <div>
+            <input type="radio" name="input" value='text'/>Text
+            <input type="radio" name="input" value='date'/>Date
+            <button onClick={this.add}>+</button>
+        </div>
+    }
+});
+
+const Left = React.createClass({
+    render:function(){
+        return <div>
+            {this.props.elements.map((ele, index)=>{
+                return <div key={index}>
+                    <input type={ele}/>
+                <button>-</button>
+                </div>
+            })}
+        </div>
     }
 });
 
