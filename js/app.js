9400379f9caa244e1b2b6354831b2f8da1387482
yangyue <m18710434142_1@163.com>
2016-08-05 02:21:40 +0800

    can change

--- js/app.js
+++ js/app.js
@@ -1,4 +1,36 @@
 const App = React.createClass({
+getInitialState:function () {
+    return{
+        elements:[],
+        isEditor:true
+    }
+},
+    toggle:function () {
+      this.setState({isEditor:!this.state.isEditor});
+    },
+   render:function () {
+    const isEditor = this.state.isEditor;
+       return <div>
+            <button onClick={this.toggle}>{isEditor?'Preview':'Editor'}</button>
+           <div>
+               <Editor />
+           </div>
+           <div>
+               <Preview />
+           </div>
+       </div>
+   }
+});
+
+const Editor = React.createClass({
+   render:function () {
+       return <div>
+
+       </div>
+   }
+});
+
+const Preview = React.createClass({
    render:function () {
        return <div>
 
