ab63ccdf70da26bc81946dda0c295172c3c08347
RangelZZZ <18792561236@163.com>
2016-08-04 22:00:25 +0800

    make the component

--- js/app.js
+++ js/app.js
@@ -1,6 +1,13 @@
 const App = React.createClass({
   render: function(){
     return <div>
+        <Editor/>
+    </div>
+  }
+});
+const Editor = React.createClass({
+  render: function(){
+    return <div>
         hello
     </div>
   }
