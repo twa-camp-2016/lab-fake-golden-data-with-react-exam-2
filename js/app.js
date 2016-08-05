bb69adace267a340ea01e5648dda662006000ab6
XLuoChen <1425145741@qq.com>
2016-08-05 09:48:02 +0800

    it can run

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
-// TODO
+const App = React.createClass({
+    render: function () {
+        return <div>
+            hello
+        </div>
+    }
+});
+
+ReactDOM.render(< App />, document.getElementById('content'));
