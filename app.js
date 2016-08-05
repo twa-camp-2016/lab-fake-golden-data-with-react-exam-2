d9cae0ef9d72efd834abb75944c7ebf2a6ddb6ff
chenyingsho <601668970@qq.com>
2016-08-05 09:42:38 +0800

    work

--- app.js
+++ app.js
@@ -2,12 +2,10 @@
  * Created by chenying on 16-8-5.
  */
 const App = React.createClass({
-
-    render: function () {
-        return <div>ddd</div>;
+    render:function () {
+        return<div>ww</div>
     }
-});
-
+})
 
 
 ReactDOM.render(<App />, document.getElementById('content'));
