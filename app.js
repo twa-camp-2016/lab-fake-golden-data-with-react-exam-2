fc7e87b67f8c3f85ba69a008d57ab22d5b15e009
chenyingsho <601668970@qq.com>
2016-08-05 08:22:15 +0800

    work

---
+++ app.js
@@ -0,0 +1,19 @@
+const App = React.createClass({
+    getInitialState:function () {
+        return{
+            isEditor:true,
+            elements:[]
+        }
+    },
+
+    render: function () {
+        return <div>eeee</div>;
+    }
+});
+
+
+
+
+
+
+ReactDOM.render(<App />, document.getElementById('content'));
