7ec104a274310c0979add962a3c532001982e524
qmm <qmmhao@sina.com>
2016-08-05 09:44:00 +0800

    can run

--- js/app.js
+++ js/app.js
@@ -1,70 +1,11 @@
  const App = React.createClass({
-  getInitialState:function(){
-    return {
-        isEditor:true,
-        elements:['rrr']
 
-    }
-  },
-     toggle:function(){
-         this.setState({isEditor:!this.state.isEditor });
-     },
    render:function(){
-      const isEditor = this.state.isEditor;
     return <div>
-        <button onClick={this.toggle}>{isEditor ? "Preview" : "Editor"}</button>
-
-        <div className={isEditor ? "" : "hidden"}>
-            <Editor elements={this.state.elements}/>
-        </div>
-
-        <div className={isEditor ? "hidden" : ""} >
-            <Previewer />
-        </div>
+        <button>Preview</button>
     </div>
    }
 
  })
 
- const Editor = React.createClass({
-
-   render:function(){
-    return    <div>
-        <Right />
-        <Left elements={this.props.elements}/>
-    </div>
-   }
- });
-
- const Left = React.createClass({
-
-   render:function(){
-    return  <div>
-        {this.props.elements}
-    </div>
-   }
- });
-
-
- const Right = React.createClass({
-
-   render:function(){
-    return  <div>
-        Right
-    </div>
-   }
-
- })
-
- const Previewer = React.createClass({
-
-   render:function(){
-    return    <div>
-        Previewer
-    </div>
-   }
-
- })
-
-
- ReactDOM.render(<App />, document.getElementById("content"))
+ ReactDOM.render(<App /> ,document.getElementById("content"));
