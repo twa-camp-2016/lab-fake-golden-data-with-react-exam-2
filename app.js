68ecdfd5eecbb0781a6bba79f831e7b27f6bead1
chenyingsho <601668970@qq.com>
2016-08-05 09:56:29 +0800

    add

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-/**
- * Created by chenying on 16-8-5.
- */
 const App = React.createClass({
     getInitialState: function () {
         return {
@@ -11,26 +8,63 @@ const App = React.createClass({
     toggle: function () {
         this.setState({
             isEditor: !this.state.isEditor
-        })
+        });
+    },
+    addElement:function (element) {
+      const elements=this.state.elements;
+        elements.push(element);
+        this.setState({elements});
     },
     render: function () {
         return <div>
             <button onClick={this.toggle}>{this.state.isEditor ? "Editor" : "Preview"}</button>
             <div className={this.state.isEditor ? "" : "hidden"}>
-                <Editor />
+                <Editor elements={this.state.elements} onadd={this.addElement}/>
             </div>
-            <div className={this.state.isEditor ? "hideen" : ""}>
+            <div className={this.state.isEditor ? "hidden" : ""}>
                 <Preview />
             </div>
         </div>;
     }
 });
+
 const Editor = React.createClass({
+
     render: function () {
-        return <div>Editor</div>
+        return <div>
+            <Left elements={this.props.elements}/>
+            <Right onadd={this.props.onadd}/>
+        </div>;
     }
 });
 
+const Left = React.createClass({
+    render: function () {
+        const elements = this.props.elements.map((ele, index)=> {
+            return <div key={index}>
+                <input type={ele}/>
+                <button>X</button>
+            </div>;
+        });
+        return <div>
+            {elements}
+        </div>
+    }
+});
+
+const Right = React.createClass({
+    add: function () {
+        const element = $("input[name=element]:checked").val();
+        this.props.onadd(element);
+    },
+    render: function () {
+        return <div>
+            <input type="radio" name="element" value="text"/>Text
+            <input type="radio" name="element" value="date"/>Date
+            <button onClick={this.add}>+</button>
+        </div>;
+    }
+});
 const Preview = React.createClass({
     render: function () {
         return <div>Preview</div>
