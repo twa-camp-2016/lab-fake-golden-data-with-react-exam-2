0a8f6d5ccec0cc84dd77d8fd263ee53c76f50db6
qmm <qmmhao@sina.com>
2016-08-05 08:46:57 +0800

    add Editor and Previewer component

--- js/app.js
+++ js/app.js
@@ -3,7 +3,33 @@
    render:function(){
     return <div>
         <button>Preview</button>
+
+        <div>
+            <Editor />
+        </div>
+
+        <div>
+            <Previewer />
+        </div>
+
     </div>
+   }
+
+ })
+
+ const Editor = React.createClass({
+
+   render:function(){
+    return   <div>
+        Editor
+    </div>
+   }
+ });
+
+ const Previewer = React.createClass({
+
+   render:function(){
+    return    <div>Previewer</div>
    }
 
  })
