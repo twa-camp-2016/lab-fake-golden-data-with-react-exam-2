9f9b32d050412f735c41e91248e3d75339bea151
yhl221 <1727538058@qq.com>
2016-08-05 09:53:13 +0800

    add toggle and editor

--- js/app.js
+++ js/app.js
@@ -1,1 +1,45 @@
-// TODO
+const App=React.createClass({
+    getInitialState:function () {
+        return{
+            isEdit:true
+        }
+    },
+
+    toggle:function () {
+        this.setState({
+            isEdit:!this.state.isEdit})
+    },
+
+    render:function(){
+        const text = this.state.isEdit ? '预览' : '编辑';
+        return <div>
+            <div>
+                <button onClick={this.toggle}>{text}</button>
+            </div>
+            <div className={this.state.isEdit?'':'hidden'}>
+                <Editor/>
+            </div>
+            <div className={this.state.isEdit?'hidden':''}>
+                <Preview/>
+            </div>
+        </div>
+    }
+});
+
+const Preview=React.createClass({
+    render:function(){
+        return <div className={this.props.className}>
+            Preview
+        </div>
+    }
+});
+
+const Editor=React.createClass({
+    render:function(){
+        return <div className={this.props.className}>
+            Editor
+        </div>
+    }
+});
+
+ReactDOM.render(<App/>,document.getElementById('content'));
